f0db876c3d294e13c097192439b528418c687d57
Alexander F <asbuka@gmail.com>
2022-05-14 08:12:50 +0300

    add validation to select array

--- ITI.QUIKAPI.MicroServices/DataValidationService/ValidateModel.cs
+++ ITI.QUIKAPI.MicroServices/DataValidationService/ValidateModel.cs
@@ -42,6 +42,40 @@ namespace DataValidationService
             return responseList;
         }
 
+        public static ListStringResponseModel ValidateMixedClientCodesArray(IEnumerable<string> code)
+        {
+            var responseList = new ListStringResponseModel();
+
+            foreach (string str in code)
+            {
+                if (str.StartsWith("C0"))
+                {
+                    ClientCodeFortsC0MatrixValidator validator = new ClientCodeFortsC0MatrixValidator();
+                    ValidationResult validationResult = validator.Validate(str);
+                    if (!validationResult.IsValid)
+                    {
+                        responseList = SetResponseFromValidationResult.SetResponse(validationResult, responseList);
+                    }
+                }
+                else if (str.StartsWith("B"))
+                {
+                    ClientCodeSpotMatrixMsMoFxRsCdRfValidator validator = new ClientCodeSpotMatrixMsMoFxRsCdRfValidator();
+                    ValidationResult validationResult = validator.Validate(str);
+                    if (!validationResult.IsValid)
+                    {
+                        responseList = SetResponseFromValidationResult.SetResponse(validationResult, responseList);
+                    }
+                }
+                else
+                {
+                    responseList.IsSuccess = false;
+                    responseList.Messages.Add($"VC100 '{str}' is not in expected formats 'BP12345-XX-01' or 'C0xxxxx'");
+                }
+            }
+
+            return responseList;
+        }
+
         public static ListStringResponseModel ValidateMatrixCDClientCodeModel(MatrixClientCodeModel model)
         {
             ClientCodeSpotMatrixCdValidator validator = new ClientCodeSpotMatrixCdValidator();
--- ITI.QUIKAPI.MicroServices/ITI.QUIKAPI.MicroServices/Controllers/QuikDataBaseController.cs
+++ ITI.QUIKAPI.MicroServices/ITI.QUIKAPI.MicroServices/Controllers/QuikDataBaseController.cs
@@ -2,6 +2,7 @@
 using DataAbstraction.Models;
 using DataAbstraction.Models.DataBaseModels;
 using DataAbstraction.Models.Responses;
+using DataValidationService;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -40,7 +41,25 @@ namespace ITI.QUIKAPI.MicroServices.Controllers
         [HttpGet("Get/RegisteredCodes")]
         public async Task<IActionResult> GetRegisteredCodesForts([FromQuery] IEnumerable<string> code)
         {
-            _logger.LogInformation("HttpGet Get/RegisteredCodes/Forts Call");
+            _logger.LogInformation("HttpGet Get/RegisteredCodes Call");
+
+            //проверим корректность входных данных
+            ListStringResponseModel validateRresult = new ListStringResponseModel();
+
+            if (code.Count() == 0)
+            {
+                validateRresult.IsSuccess = false;
+                validateRresult.Messages.Add("Get/RegisteredCodes code must contain at least 1 code");
+                return BadRequest(validateRresult);
+            }
+
+            validateRresult = ValidateModel.ValidateMixedClientCodesArray(code);
+            if (!validateRresult.IsSuccess)
+            {
+                _logger.LogInformation($"HttpGet GetAllClientsFromTemplate/PoKomissii Error: {validateRresult.Messages[0]}");
+                return BadRequest(validateRresult);
+            }
+
 
             DataBaseClientCodesResponse result = await _repository.GetRegisteredCodes(code);
 
